c61dd8d9adeef4bfcf623f7baae0a8d21aea39a0
Eva Charizanou <charizanouev@gmail.com>
2026-09-22 09:41:02 +0000

    Add cents_to_euros macro, apply to staging

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -8,5 +8,5 @@ select
     lower(method)          as payment_method,
     lower(payment_status)  as payment_status,
     amount_cents,
-    round(amount_cents / 100.0, 2) as amount_eur
-from source
+    {{ cents_to_euros('amount_cents') }} as amount_eur
+    from source
